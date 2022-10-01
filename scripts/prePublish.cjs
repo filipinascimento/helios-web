69ac900f0b8816da98cc2d1007bb20962c1346cf
filipinascimento <filipinascimento@gmail.com>
2022-10-01 06:39:29 +0000

    Deploying to gh-pages from @ filipinascimento/helios-web@0a5c3a9e8ce3d2974780599002faa153916e455c 🚀

--- scripts/prePublish.cjs
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-let log = (err, stdout, stderr) => console.log(stdout)
-fs.rm("build", { recursive: true,force:true, maxRetries:10}, function(err, result) {
-  if(err) console.log('error', err);
-});
